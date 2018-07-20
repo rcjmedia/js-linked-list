7b03d1c545719dcafd9d51a8de07ae6d86a23cf8
Romeo <romeocorpuz808@gmail.com>
2018-07-19 21:50:47 -1000

    first commit

--- linkedList.js
+++ linkedList.js
@@ -4,5 +4,37 @@
  * @return {Object} an object exposing methods to be used to manipulate a linked list
  */
 function linkedListGenerator(){
-
-}
+    var head = null;
+    var tail = null;
+  
+    return {
+      getHead : getHead,
+      getTail: getTail,
+      add: add
+    };
+  
+    function getHead() {
+      return head;
+    }
+  
+    function getTail() {
+      return tail;
+    }
+    
+    function add(valz) {
+        let nodeNew = {
+          value : valz,
+          next : null
+        };
+    
+        if (!head) {      
+          head = nodeNew;
+          tail = nodeNew;
+    
+        } else {
+          tail.next = nodeNew;
+          tail = nodeNew;
+        }
+        return tail;
+      }
+  }
